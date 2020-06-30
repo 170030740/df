02b15a6e0afc0e62928f44681d0340cddedd9e95
170030740 <66765603+170030740@users.noreply.github.com>
2020-06-30 17:04:10 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.lang.*;
+public class mul {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, mul;
+      multi = num1 * num2;
+
+      System.out.println("multi of these numbers: "+multi);
+   }
+}
